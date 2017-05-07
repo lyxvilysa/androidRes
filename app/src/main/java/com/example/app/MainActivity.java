f847fcd83d660e1acbdb410073ff6c812aa76558
lyxvilysa <1439045315@qq.com>
2017-05-07 15:58:31 +0800

    the second explan

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //加入一行注释
+        //the second explan
     }
 }
